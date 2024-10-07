3f2560b57bc8a65ede745bb706b0b1f75bb8371d
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2024-10-07 22:38:23 +0530

    fix: path errors and add basic DL test code

--- src/dataset.py
+++ src/dataset.py
@@ -1,9 +1,9 @@
-import os
+import os, sys
 import random
 from typing import List
 from utils import pkl_load
 
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import torch
 
 
@@ -11,13 +11,13 @@ class OurDataset(Dataset):
     def __init__(self, dataroot: str, max_len: int, type: str):
         def resolve_paths() -> List[str]:
             """
-            returns absolute paths of all files associated with the dataset (train/test/val)
+            returns absolute paths of all files associated with the dataset (train/test/valid)
             resolved against the root path provided
             """
 
             files = (
-                "idx_to_word",
-                "word_to_idx",
+                "index_to_word",
+                "word_to_index",
                 "src",
                 "src_bert_ids",
                 "src_tags",
@@ -27,24 +27,27 @@ class OurDataset(Dataset):
                 "similarity"
             )
 
+            dirname = os.path.dirname(__file__)
             abspaths = []
             for file in files:
-                is_vocab_file = file in ("idx_to_word", "word_to_idx")
-                relpath = dataroot + "/" + (f"{self.type}_" if is_vocab_file else "") + file + ".pkl"
-                abspaths.append(os.path.join(relpath))
+                is_vocab_file = file in ("index_to_word", "word_to_index")
+                relpath = dataroot + "/" + (f"{self.type}_" if not is_vocab_file else "") + file + ".pkl"
+                abspath = os.path.join(dirname, relpath)
+                sys.path.append(abspath) # TODO: maybe there's a better way to handle this
+                abspaths.append(abspath)
             return abspaths
         
 
         super().__init__()
 
-        assert type in ["train", "test", "val"]
+        assert type in ["train", "test", "valid"]
         self.type = type
         self.max_len: int = max_len
 
         abspaths = resolve_paths()
 
-        self.idx_to_word = pkl_load(abspaths[0])
-        self.word_to_idx = pkl_load(abspaths[1])
+        self.index_to_word = pkl_load(abspaths[0])
+        self.word_to_index = pkl_load(abspaths[1])
         self.src = pkl_load(abspaths[2])
         self.src_bert_ids = pkl_load(abspaths[3])
         self.src_tags = pkl_load(abspaths[4])
@@ -52,6 +55,8 @@ class OurDataset(Dataset):
         self.trg_bert_ids = pkl_load(abspaths[6])
         self.trg_tags = pkl_load(abspaths[7])
         self.similarity = pkl_load(abspaths[8])
+
+        self.vocab_size = len(self.word_to_index)
 
 
     def __len__(self) -> int:
@@ -81,21 +86,6 @@ class OurDataset(Dataset):
         # seq2seq data_quora format
         src_sent = self.src[idx]
         trg_sent = self.trg[idx]
-        # src_len = len(src_sent)
-        # trg_len = len(trg_sent)
-
-        # src, src_len = trim_tensor(src_sent, self.max_len)
-        # content_trg, content_len = trim_tensor(trg_sent, self.max_len)
-        # trg, trg_len = trim_tensor(trg_sent, self.max_len, append=3) # EOS
-        # trg_input, trg_len = trim_tensor(trg_sent, self.max_len, prepend=2)
-
-        # # bert data_quora format
-        # bert_in = self.src_bert_ids[idx]
-        # bert_out = self.trg_bert_ids[idx]
-        # exmp = self.trg_bert_ids[random.choice(self.similarity[idx])]
-        # bert_src, _ = trim_tensor(bert_in, self.max_len + 2)
-        # bert_trg, _ = trim_tensor(bert_out, self.max_len + 2)
-        # bert_exmp, _ = trim_tensor(exmp, self.max_len + 2)
 
         src = trim_tensor(src_sent, self.max_len)
         content_trg = trim_tensor(trg_sent, self.max_len)
@@ -110,7 +100,57 @@ class OurDataset(Dataset):
         bert_trg = trim_tensor(bert_out, self.max_len + 2)
         bert_exmp = trim_tensor(exmp, self.max_len + 2)
 
-        # return src, src_len, trg, trg_input, trg_len, bert_src, bert_trg, bert_exmp, content_trg, content_len
-        return src, trg, trg_input, bert_src, bert_trg, bert_exmp, content_trg
+        return src, trg, content_trg, trg_input, bert_src, bert_trg, bert_exmp
+
+
+if __name__ == "__main__":
+    # some basic code to test if the dataset is working fine
+
+    train = OurDataset("../data/quora/processed", 15, "train")
+    val = OurDataset("../data/quora/processed", 15, "valid")
+    test = OurDataset("../data/quora/processed", 15, "test")
+
+    train_dl = DataLoader(train, batch_size=64, shuffle=False)
+    val_dl = DataLoader(val, batch_size=64, shuffle=False)
+    test_dl = DataLoader(test, batch_size=64, shuffle=False)
+
+    count = 0
+    for data in train_dl:
+        if count >= 10:
+            break
+        count += 1
+        print(count, end="\t")
+        print([d.shape for d in data])
+
+    print("*" * 10)
+    count = 0
+    for data in test_dl:
+        if count >= 10:
+            break
+        count += 1
+        print(count, end="\t")
+        print([d.shape for d in data])
+
+    print("*" * 10 )
+    count = 0
+    for data in val_dl:
+        if count >= 10:
+            break
+        count += 1
+        print(count, end="\t")
+        print([d.shape for d in data])
+    
+    print("*" * 10 )
+    count = 0
+    for data in train_dl:
+        if count >= 1:
+            break
+        count += 1
+        print(count, end="\t")
+        for d in data:
+            print(d.shape, d)
+            print("*" * 10 )
+            
+    
 
 
